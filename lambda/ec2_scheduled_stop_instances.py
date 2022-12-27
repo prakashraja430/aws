d2f3cba76e6df1450dd2743eda03e8a6a744287d
Prakash Raja <76421701+prakashraja430@users.noreply.github.com>
2022-12-27 12:40:03 +0530

    Updated based on lambda events
    Updated the code based on the Lambda events.

--- lambda/ec2_scheduled_stop_instances.py
+++ lambda/ec2_scheduled_stop_instances.py
@@ -1,25 +1,30 @@
 import boto3
+import logging
 # Enter the region 
-ec2client = boto3.client('ec2',region_name='us-east-2')
+ec2client = boto3.client('ec2',region_name='<AWS-Region>')
 response = ec2client.describe_instances()
+# Setting logging level to INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# getting the list of running instances
-running_instances=[]
-for reservation in response["Reservations"]:
-    for instance in reservation["Instances"]:
-        if instance['State']['Name'] == 'running':
-           x = (instance["InstanceId"])
-           running_instances.append(x)
-
-# Removing the whitelisted instance from the running instance list
-# Enter the instances to whitelist from the running instance
-# Ex: whitelist_instances=['i-1a2b3c4d5e6f7g','i-8h9i10j11k12l']
-whitelist_instances=[]
-for n in whitelist_instances:
-    while n in running_instances:
-        running_instances.remove(n)
-
-# ec2 instance stopping code
-for n in running_instances:
-    ec2client.stop_instances(InstanceIds=[n])
-    print('stopping instance: '+ n)
+def lambda_handler(event, context):
+    running_instances=[]
+    for reservation in response["Reservations"]:
+       for instance in reservation["Instances"]:
+            if instance['State']['Name'] == 'running':
+              x = (instance["InstanceId"])
+              running_instances.append(x)
+    # Removing the whitelisted instance from the running instance list
+    # Enter the instances to whitelist from the running instance
+    # Ex: whitelist_instances=['i-1a2b3c4d5e6f7g','i-8h9i10j11k12l']
+    whitelist_instances=[]
+    for n in whitelist_instances:
+        while n in running_instances:
+            running_instances.remove(n)
+    if not running_instances:
+        logger.info('No instance to stop')
+    if running_instances:
+        # ec2 instance stopping
+        for n in running_instances:
+            ec2client.stop_instances(InstanceIds=[n])
+            logger.info('stopping instance: ' + n)
